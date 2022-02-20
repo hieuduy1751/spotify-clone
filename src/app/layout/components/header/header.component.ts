5a9f79e4acd9b387d25643ffa180d5b0e89ef123
Hieu Nguyen Duy <hieuduy1751@gmail.com>
2022-02-20 17:34:20 +0700

    finish header UI

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,5 +1,6 @@
+import { User } from './../../../spotify/models/user/user';
 import { NavigatorState } from './../../../spotify/states/navigator/navigator.state';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,10 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @ViewChild('header') header!: ElementRef;
+  @ViewChild('account') accountERef!: ElementRef;
+  @Input() me!: User;
+  profileControlStatus = false;
+
   constructor(public navigator: NavigatorState) {
   }
 
@@ -20,5 +25,12 @@ export class HeaderComponent implements OnInit {
 
   forward() {
     this.navigator.forward();
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOut(event: any) {
+    if (!this.accountERef.nativeElement.contains(event.target)) {
+      this.profileControlStatus = false;
+    }
   }
 }
