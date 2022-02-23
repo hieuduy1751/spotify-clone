d5bc096292e0c34c2513904421125b1a7b609d88
Duy Hieu <62537918+hieuduy1751@users.noreply.github.com>
2022-02-24 02:02:12 +0700

    Revert "del duplicated file"
    This reverts commit bdf9c59abf8166bd821fe6e90566fe0c1c9785cf.

---
+++ src/components/aside/aside.component.ts
@@ -0,0 +1,32 @@
+import { PlaylistState } from '../../app/spotify/states/playlist/playlist.state';
+import { Component, Input, OnInit } from '@angular/core';
+import { AsideMenu } from '../../app/spotifyUI/configs/aside-menu.config'
+import { Playlist } from 'src/app/spotify/models/playlist/playlist';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.scss']
+})
+export class AsideComponent implements OnInit {
+  @Input() menu!: AsideMenu[];
+  @Input() buttons!: AsideMenu[];
+  myPlaylists: Playlist[] = [];
+
+  constructor(private playlistState: PlaylistState) { }
+
+  ngOnInit(): void {
+    this.listenStateEvent();
+  }
+
+  listenStateEvent(): void {
+    this.playlistState.playlists$.subscribe(playlists => this.playlistsChange());
+  }
+
+  playlistsChange(): void {
+    const playlists = this.playlistState.getMyPlaylist();
+    if (playlists) {
+      this.myPlaylists = playlists;
+    }
+  }
+}
