785c142039618f0c7e7d5730091d74e19e39698e
Duy Hieu <62537918+hieuduy1751@users.noreply.github.com>
2022-02-24 02:02:12 +0700

    Revert "finish navigator"
    This reverts commit 99008f7ce324f03fbc7aa9d417e5ac50e341623a.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,7 +1,5 @@
-import { PlaylistState } from './../spotify/states/playlist/playlist.state';
-import { HeaderComponent } from './components/header/header.component';
 import { UserState } from './../spotify/states/user/user.state';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AsideMenu, AsideMenuConfig } from '../spotifyUI/configs/aside-menu.config';
 import { User } from '../spotify/models/user/user';
 
@@ -11,13 +9,11 @@ import { User } from '../spotify/models/user/user';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-  @ViewChild(HeaderComponent)
-  header!: HeaderComponent;
   asideWidth = 20;
   asideMenu: AsideMenu[] = AsideMenuConfig.items;
   buttons: AsideMenu[] = AsideMenuConfig.buttons;
   me: User = new User();
-  constructor(private userState: UserState, private playlistState: PlaylistState) {
+  constructor(private userState: UserState) {
     this.userState.me$.subscribe((me) => {
       this.me = me;
     });
@@ -27,16 +23,5 @@ export class LayoutComponent implements OnInit {
   }
 
   ngAfterViewInit() {
-
-  }
-
-  onScroll(event: any) {
-    let color = this.playlistState.getColorDetected();
-
-    if (color.colors.accent) {
-      this.header.header.nativeElement.style.backgroundColor = `rgba(${color.colors.dominant.r}, ${color.colors.dominant.g}, ${color.colors.dominant.b}, ${event.target.scrollTop / 100})`;
-    } else {
-      this.header.header.nativeElement.style.backgroundColor = `rgba(33, 33, 33, ${event.target.scrollTop / 100})`;
-    }
   }
 }
